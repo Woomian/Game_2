33dec1482d291f86665c066678bd24656efe09c0
Woomian <doherty-f15@ulster.ac.uk>
2024-12-13 14:46:42 +0000

    changed file names and finalised game

---
+++ Assets/bounceMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounceMovement : MonoBehaviour
+{
+    public float bounceForce = 10f;
+    public int damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            HandlePlayerBounce(collision.gameObject);
+        }
+    }
+
+    private void HandlePlayerBounce(GameObject player)
+    {
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+
+        if (rb)
+        {
+            //reset player velocity
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+
+            //apply bounce force
+            rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
